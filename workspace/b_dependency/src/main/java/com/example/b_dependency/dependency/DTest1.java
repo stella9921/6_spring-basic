04726e366a1b44746185dbe1db536330f02a2451
stella9921 <stella9921@naver.com>
2024-05-28 21:46:39 +0900

    5/28

---
+++ workspace/b_dependency/src/main/java/com/example/b_dependency/dependency/DTest1.java
@@ -0,0 +1,16 @@
+package com.example.b_dependency.dependency;
+
+
+import org.springframework.stereotype.Component;
+
+//스프링 컨테이너 에 등록해줘!
+@Component
+public class DTest1 {
+
+    public void print(){
+        System.out.println("DTest1");
+
+
+
+    }
+}
